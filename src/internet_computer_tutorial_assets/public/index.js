d05a24de519de251318676abd50bab05e68a2425
Mitul Patel <mitulpatel.hsd@gmail.com>
2021-04-02 00:28:45 -0700

    Changed prompt message

--- src/internet_computer_tutorial_assets/public/index.js
+++ src/internet_computer_tutorial_assets/public/index.js
@@ -1,5 +1,5 @@
 import internet_computer_tutorial from 'ic:canisters/internet_computer_tutorial';
 
-internet_computer_tutorial.greet(window.prompt("Enter your name:")).then(greeting => {
+internet_computer_tutorial.greet(window.prompt("Enter your AMAZING name:")).then(greeting => {
   window.alert(greeting);
 });
